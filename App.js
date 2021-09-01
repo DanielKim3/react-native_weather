4ffbde640169fd3e57721df265e91691e08b505f
kimwoojin <wjinkim21@naver.com>
2021-09-02 01:09:25 +0900

    finally

--- App.js
+++ App.js
@@ -1,45 +1,51 @@
 import React from "react";
-import {Alert} from "react-native";
+import { Alert } from "react-native";
 import Loading from "./Loading";
-import * as Location from 'expo-location';
+import * as Location from "expo-location";
 import axios from "axios";
+import Weather from "./Weather";
 
-const API_KEY ="7af99e4b7fafdaedcf6ecaf1e698db62"
+const API_KEY = "7af99e4b7fafdaedcf6ecaf1e698db62";
 
-export default class extends React.Component { 
-    
-      state = {
-        isLoading : true
-      };
-      getWeather = async(latitude, longitude) => {
-        const {
-          data: {
-            main: {temp},
-            weather
-            }
-        } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`); //`백틱을 쓰는 이유는 변수를 문자열에다 포함시킬꺼기 때문
-        this.setState({isLoading:false, condition:  weather[0].main, temp
-      });
-    };
-      getLocation = async() => {
-      try{      
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  };
+  getWeather = async (latitude, longitude) => {
+    const {
+      data: {
+        main: { temp },
+        weather
+      }
+    } = await axios.get(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&APPID=${API_KEY}&units=metric`
+    );
+    this.setState({
+      isLoading: false,
+      condition: weather[0].main,
+      temp
+    });
+  };
+  getLocation = async () => {
+    try {
       await Location.requestBackgroundPermissionsAsync();
-      console.log(response);      
-      const {coords : { latitude, longitude} } = await Location.getCurrentPositionAsync();
-      //send to API and get weather
-      this.getWeather(latitude, longitude)
-      this.state({isLoading:false});
-    } catch (error){
-      Alert.alert("caa't find you", "So sad");
+      const {
+        coords: { latitude, longitude }
+      } = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert("Can't find you.", "So sad");
     }
   };
-    componentDidMount(){
-      this.getLocation();
-    };
-  render(){
-    const {isLoading, temp, condition} = this.state;
-    return isLoading ? <Loading /> : <Weather temp={Math.round(temp)} condition={condition}/>;
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading, temp, condition } = this.state;
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Weather temp={Math.round(temp)} condition={condition} />
+    );
   }
 }
-
-
